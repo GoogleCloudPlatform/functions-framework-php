041f1055061eca643b159ab38f0360dc17eb05a9
Grant Timmerman <timmerman+devrel@google.com>
2020-01-08 13:41:18 +0700

    fixes #18 add config defaults

--- router.php
+++ router.php
@@ -59,7 +59,7 @@ if ($functionSource = getenv('FUNCTION_SOURCE', true)) {
 (function () {
     $target = getenv('FUNCTION_TARGET', true);
     if (false === $target) {
-        throw new RuntimeException('FUNCTION_TARGET is not set');
+        $target = 'function';
     }
     if (!is_callable($target)) {
         throw new InvalidArgumentException(sprintf(
@@ -67,7 +67,7 @@ if ($functionSource = getenv('FUNCTION_SOURCE', true)) {
     }
     $signatureType = getenv('FUNCTION_SIGNATURE_TYPE', true);
     if (false === $signatureType) {
-        throw new RuntimeException('FUNCTION_SIGNATURE_TYPE is not set');
+        $signatureType = 'http'
     }
 
     $invoker = new Google\CloudFunctions\Invoker($target, $signatureType);
