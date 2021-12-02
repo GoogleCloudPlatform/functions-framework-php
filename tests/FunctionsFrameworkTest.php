41ab52514051df9976285fa788a7e7239dec7715
Brent Shaffer <betterbrent@google.com>
2021-12-02 13:31:55 -0500

    feat: add class for testing (#119)

---
+++ tests/FunctionsFrameworkTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright 2021 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\CloudFunctions\Tests;
+
+use Google\CloudFunctions\FunctionsFramework;
+use Google\CloudFunctions\FunctionsFrameworkTesting;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use CloudEvents\V1\CloudEventInterface;
+
+/**
+ * @group gcf-framework
+ */
+class FunctionsFrameworkTest extends TestCase
+{
+    public function testRegisterAndRetrieveHttpFunction(): void
+    {
+        $fn = function (ServerRequestInterface $request) {
+            return "this is a test function";
+        };
+
+        FunctionsFramework::http('testFn', $fn);
+
+        $this->assertEquals(
+            $fn,
+            FunctionsFrameworkTesting::getRegisteredFunction('testFn')
+        );
+    }
+
+    public function testRegisterAndRetrieveCloudEventFunction(): void
+    {
+        $fn = function (CloudEventInterface $event) {
+            return "this is a test function";
+        };
+
+        FunctionsFramework::cloudEvent('testFn', $fn);
+
+        $this->assertEquals(
+            $fn,
+            FunctionsFrameworkTesting::getRegisteredFunction('testFn')
+        );
+    }
+
+    public function testRetrieveNonexistantFunction(): void
+    {
+        $this->assertNull(
+            FunctionsFrameworkTesting::getRegisteredFunction('thisDoesntExist')
+        );
+    }
+}
